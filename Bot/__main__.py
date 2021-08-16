4b9fb604f405d2e2bfd76f814fcb34b3b6851b47
Saurabh <sauravvishwakarma030@gmail.com>
2021-08-16 14:04:51 +0530

    Added few codes

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -2,7 +2,10 @@ from discord.ext import commands
 import discord
 import os 
 from dotenv import load_dotenv
-import embeds
+from embeds import Embeds
+
+load_dotenv()
+token = os.getenv("TOKEN")
 
 client = discord.Client()
 worker = Embeds()
@@ -11,4 +14,9 @@ async def on_ready():
   print("The bot is ready by user {}".format(client))
 @client.event
 async def on_message(message):
-  if message.content.startswith("up!quote"):
+  if message.content.startswith("up!help"):
+    help_text = worker._help()
+    await message.channel.send(embed=help_text)
+    
+    
+client.run(token)
