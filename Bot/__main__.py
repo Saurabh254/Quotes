8ba3157a91538c3c0ca2543eadf21ff876df560e
Saurabh <sauravvishwakarma030@gmail.com>
2021-08-16 18:59:04 +0530

    In main file added Quote command

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -1,9 +1,10 @@
 from discord.ext import commands
 import discord
 import os 
+import time
 from dotenv import load_dotenv
 from embeds import Embeds
-from QJsonFormatter import Quote, runner
+from QJsonFormatter import _Quotes_
 import asyncio
 
 load_dotenv()
@@ -20,13 +21,22 @@ async def on_message(message):
     help_text = worker._help()
     await message.channel.send(embed=help_text)
   if message.content.startswith("up!ping"):
-    latency = round(client.latency * 1000)
+    latency = round(client.latency * 100)
     await message.channel.send(embed=worker.ping(latency))
   if message.content.startswith("up!quote"):
-    quote = Quote()
-    jsonL =  runner(quote)
-    q_text = quote.Text_Quotes(jsonL)
+    quote =  _Quotes_()
+    jsonL = quote._response_()
+    text_quote = quote.Json_quote(jsonL)
     author = quote.author_ofQuote(jsonL)
-    await message.channel.send(embed=worker._quote_(q_text, author))
+    await message.channel.send(embed=worker._quote_(text_quote, author))
+  if message.content.startswith("up!start"):
+    while True:
+      quote =  _Quotes_()
+      jsonL = quote._response_()
+      text_quote = quote.Json_quote(jsonL)
+      author = quote.author_ofQuote(jsonL)
+      time.sleep(10)
+      await message.channel.send(embed=worker.loop_quote_(text_quote, author))
+    
     
 client.run(token)
