466f577672ef99950a2b84cb1bc3307729c7cc27
Saurabh <sauravvishwakarma030@gmail.com>
2021-08-16 18:58:11 +0530

    In Json formatter replaced aiohttp module to request module

--- Bot/QJsonFormatter.py
+++ Bot/QJsonFormatter.py
@@ -1,21 +1,16 @@
-import aiohttp
 import asyncio
-import time
 import json
+import requests
 
-
-class Quote:
-  async def fetch_json(self):
-    async with aiohttp.ClientSession() as session:
-      response = await session.get("http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en")
-      rawJson = await response.text()
-      jsonL = json.loads(rawJson)
-      return jsonL
-  def Text_Quotes(self,jsonL):
+class _Quotes_:
+  def _response_(self):
+    url = "http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en"
+    response = requests.get(url)
+    jsonL = json.loads(response.content)
+    return jsonL
+  def Json_quote(self, jsonL):
     Qtext = jsonL.get("quoteText")
     return Qtext
-  def author_ofQuote(self,jsonL):
+  def author_ofQuote(self, jsonL):
     author = jsonL.get("quoteAuthor")
     return author
-def runner(quote):
-  asyncio.run(quote.fetch_json())
