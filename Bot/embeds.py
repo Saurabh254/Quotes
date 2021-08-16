04afc5dac028536b3f59717c19c83a84589719df
Saurabh <sauravvishwakarma030@gmail.com>
2021-08-16 12:18:08 +0530

    Incomplete embeds added

---
+++ Bot/embeds.py
@@ -0,0 +1,11 @@
+class Embeds:
+  def ping(self):
+    embedVar = discord.Embed(title="Title", description="Desc", color=0x00ff00)
+    embedVar.add_field(name="Field1", value="hi", inline=False)
+    embedVar.add_field(name="Field2", value="hi2", inline=False)
+    return embedVar
+  
+  def _help(self):
+    embedVar = discord.Embed(title="Title", description="Desc", color=0x00ff00)
+    embedVar.add_field(name="Field1", value="hi", inline=False)
+    embedVar.add_field(name="Field2", value="hi2", inline=False)
